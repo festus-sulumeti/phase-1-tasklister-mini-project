0f745a27a23c51b97a3d621d578273fad595bc6d
festus-sulumeti <sulumetifestus@outlook.com>
2023-10-01 16:08:09 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const dom = document;
+
+  let form = dom.getElementById("create-task-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let ul = dom.getElementById("tasks");
+    let li = dom.createElement("li");
+    li.innerHTML = event.target[0].value;
+    ul.appendChild(li);
+    event.target.reset();
+  }); 
+
+  let button = dom.getElementById("delete");
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    let ul = dom.getElementById("tasks");
+    let li = ul.lastElementChild;
+    li.remove();
+  });
+
+  let button2 = dom.getElementById("delete-all");
+
+  button2.addEventListener("click", function (event) {
+    event.preventDefault();
+    let ul = dom.getElementById("tasks");
+    let li = ul.lastElementChild;
+    while (li) {
+      ul.removeChild(li);
+      li = ul.lastElementChild;
+    }
+  });
+  
+
 });
